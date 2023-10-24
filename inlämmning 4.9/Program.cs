160a176b8fdb0a11012934755a49d6f3862cc1fa
alsv06003 <alsv06003@E-PW0234JW>
2023-10-24 10:26:42 +0200

    Add project files.

---
+++ inlämmning 4.9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace inlämmning_4._9
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många X per grupp?");
+            int antalX = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många O? per grupp");
+            int antalO = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många O-grupper per rad?");
+            int antalGånger = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många rader?");
+            int antalRader = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < antalRader; i++)
+            {
+                for (int j = 0; j < antalGånger; j++)
+                {
+                    for (int k = 0; k < antalX; k++)
+                    {
+                        Console.Write("X");
+                    }
+                    Console.Write("-");
+
+                    for (int k = 0; k < antalO; k++)
+                    {
+                        Console.Write("O");
+                    }
+                    Console.Write("-");
+                }
+                for (int k = 0; k < antalX; k++)
+                {
+                    Console.Write("X");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
